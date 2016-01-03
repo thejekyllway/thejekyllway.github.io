3e6cf711e72632370f9df4c81b5e3ccc26d65c52
Anderson Menezes <andersomxp@gmail.com>
2016-01-03 19:24:46 -0300

    Adicionando script para publicações

---
+++ initpost.sh
@@ -0,0 +1,167 @@
+#!/usr/bin/env bash
+
+# ------------------------------------------------------------------------------
+#
+# Program: initpost.sh
+# Author:  Vitor Britto
+# Description: script to create an initial structure for my posts.
+#
+# Usage: ./initpost.sh [options] <post name>
+#
+# Options:
+#   -h, --help        output instructions
+#   -c, --create      create post
+#
+# Alias: alias ipost="bash ~/path/to/script/initpost.sh"
+#
+# Example:
+#   ./initpost.sh -c How to replace strings with sed
+#
+# Important Notes:
+#   - This script was created to generate new markdown files for my blog.
+#
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+# | VARIABLES                                                                  |
+# ------------------------------------------------------------------------------
+
+# CORE: Do not change these lines
+# ----------------------------------------------------------------
+POST_TITLE="${@:2:$(($#-1))}"
+POST_NAME="$(echo ${@:2:$(($#-1))} | sed -e 's/ /-/g' | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/")"
+CURRENT_DATE="$(date +'%Y-%m-%d')"
+FILE_NAME="${CURRENT_DATE}-${POST_NAME}.md"
+# ----------------------------------------------------------------
+
+
+# SETTINGS: your configuration goes here
+# ----------------------------------------------------------------
+
+# Set your destination folder
+DIST_FOLDER="$HOME/Documents/Projects/the-jekyll-way/_posts/"
+
+# Set your blog URL
+BLOG_URL=""
+
+# Set your assets URL
+ASSETS_URL="src/image/authors"
+# ----------------------------------------------------------------
+
+
+
+# ------------------------------------------------------------------------------
+# | UTILS                                                                      |
+# ------------------------------------------------------------------------------
+
+# Header logging
+e_header() {
+    printf "$(tput setaf 38)→ %s$(tput sgr0)\n" "$@"
+}
+
+# Success logging
+e_success() {
+    printf "$(tput setaf 76)✔ %s$(tput sgr0)\n" "$@"
+}
+
+# Error logging
+e_error() {
+    printf "$(tput setaf 1)✖ %s$(tput sgr0)\n" "$@"
+}
+
+# Warning logging
+e_warning() {
+    printf "$(tput setaf 3)! %s$(tput sgr0)\n" "$@"
+}
+
+
+
+# ------------------------------------------------------------------------------
+# | MAIN FUNCTIONS                                                             |
+# ------------------------------------------------------------------------------
+
+# Everybody need some help
+initpost_help() {
+
+cat <<EOT
+
+------------------------------------------------------------------------------
+INIT POST - A shortcut to create an initial structure for my posts.
+------------------------------------------------------------------------------
+
+Usage: ./initpost.sh [options] <post name>
+
+Options:
+  -h, --help        output instructions
+  -c, --create      create post
+
+Example:
+  ./initpost.sh -c How to replace strings with sed
+
+Important Notes:
+  - This script was created to generate new text files to my blog.
+
+
+Copyright (c) Vitor Britto
+Licensed under the MIT license.
+
+------------------------------------------------------------------------------
+
+EOT
+
+}
+
+# Initial Content
+initpost_content() {
+
+echo "---"
+echo "layout: post"
+echo "title:"
+echo "description:"
+echo "date: ${CURRENT_DATE}"
+echo "img: \"${ASSETS_URL}/\""
+echo "link:"
+echo "comments: true"
+echo "---"
+
+}
+
+# Create file
+initpost_file() {
+    if [ ! -f "$FILE_NAME" ]; then
+        e_header "Creating template..."
+        initpost_content > "${DIST_FOLDER}/${FILE_NAME}"
+        open "${DIST_FOLDER}/${FILE_NAME}"
+        e_success "Initial post successfully created!"
+    else
+        e_warning "File already exist."
+        exit 1
+    fi
+
+}
+
+
+
+# ------------------------------------------------------------------------------
+# | INITIALIZE PROGRAM                                                         |
+# ------------------------------------------------------------------------------
+
+main() {
+
+    # Show help
+    if [[ "${1}" == "-h" || "${1}" == "--help" ]]; then
+        initpost_help ${1}
+        exit
+    fi
+
+    # Create
+    if [[ "${1}" == "-c" || "${1}" == "--create" ]]; then
+        initpost_file $*
+        exit
+    fi
+
+}
+
+# Initialize
+main $*
